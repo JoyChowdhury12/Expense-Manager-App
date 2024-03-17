60a2e0f7bdd9ad747c22b7cb6aa37cda4ba3926f
Joy Chowdhury <147070186+JoyChowdhury12@users.noreply.github.com>
2024-03-17 11:04:43 +0600

    Added Dark mode features

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,22 @@ import 'package:flutter/material.dart';
 var kColourScheme =
     ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 96, 59, 181));
 
+var kDarkColourScheme = ColorScheme.fromSeed(
+    brightness: Brightness.dark, seedColor: Color.fromARGB(255, 5, 99, 125));
+
 void main() {
   runApp(MaterialApp(
+    darkTheme: ThemeData.dark().copyWith(
+      useMaterial3: true,
+      cardTheme: CardTheme().copyWith(
+          color: kDarkColourScheme.secondaryContainer,
+          margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16)),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColourScheme.primaryContainer,
+            foregroundColor: kDarkColourScheme.onPrimaryContainer),
+      ),
+    ),
     theme: ThemeData().copyWith(
         useMaterial3: true,
         colorScheme: kColourScheme,
--- lib/widgets/expense_list.dart
+++ lib/widgets/expense_list.dart
@@ -15,6 +15,11 @@ class ExpenseList extends StatelessWidget {
     return ListView.builder(
       itemCount: expenses.length,
       itemBuilder: (context, index) => Dismissible(
+        background: Container(
+          color: Theme.of(context).colorScheme.error.withOpacity(0.75),
+          margin: EdgeInsets.symmetric(
+              horizontal: Theme.of(context).cardTheme.margin!.horizontal),
+        ),
         onDismissed: (direction) {
           onRemoveExpense(expenses[index]);
         },
