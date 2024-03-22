8fe7b459d4cdffebdeb9a1d1aa128097085d6db5
Joy Chowdhury <147070186+JoyChowdhury12@users.noreply.github.com>
2024-03-22 13:20:03 +0600

    Modified some things on the chart bar

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -48,6 +48,7 @@ class Chart extends StatelessWidget {
         children: [
           Expanded(
             child: Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
               children: [
                 for (final bucket in buckets)
                   ChartBar(
